937481602e95c92b5c2c7ace7764e89baedbe353
ListingGenie2157 <listnggenie@gmail.com>
2025-09-15 03:14:46 -0500

    Create index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,17 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export { Label } from "./label";
+export {
+  // Radix Select exports (shadcn pattern)
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectScrollDownButton,
+  SelectScrollUpButton
+} from "./select";
+export { Switch } from "./switch";
